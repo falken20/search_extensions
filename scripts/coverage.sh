30fde03139e0d5dfc6468eb438bf03ce8ea738e6
Richi <r.rodriguez@bbva.com>
2022-02-18 23:54:23 +0100

    Workflow action for GitHub

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-coverage run -m pytest -v && coverage html --omit=*/venv/*,*/test/*
+coverage run -m pytest -v && coverage report --omit="*/test/*,*/venv/*"
+
+# Coverage report in html
+# coverage run -m pytest -v && coverage html --omit="*/test/*,*/venv/*"
 
 # With param -s for input
-# coverage run -m pytest - v -s && coverage html --omit=*/venv/*,*/test/*
+# coverage run -m pytest - v -s && coverage html --omit="*/test/*,*/venv/*"
